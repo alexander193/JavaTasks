1cff4e5dec5397da7844a6e62c4a38299514af74
alexander193 <sana2001@list.ru>
2023-11-08 11:56:27 +0300

    taskString done

--- src/taskString/taskString.java
+++ src/taskString/taskString.java
@@ -10,10 +10,13 @@ package taskString;
 public class taskString {
     public static void main(String[] args) {
         String[] array = {"a", "as", "aasdasdsd", "asdf", "asdfg"};
-        System.out.println(searchThwLongest(array));
+        System.out.println(searchTheLongest(array));
+        System.out.println(isPalindrome("ШаЛаш"));
+        System.out.println(wordReplacer("сука бля"));
+        System.out.println(checkRepeats("привет сука привет", "привет"));
     }
 
-    public static String searchThwLongest(String[] in) {
+    public static String searchTheLongest(String[] in) {
         int maxLength = 0, longestLine = -1;
         for (int i = 0; i < in.length; i++) {
             if (in[i].length() > maxLength) {
@@ -22,5 +25,40 @@ public class taskString {
             }
         }
         return in[longestLine];
+    }
+    public static boolean isPalindrome(String in) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = in.length() - 1; i >= 0; i--)
+            reverse.append(in.charAt(i));
+        return reverse.toString().equalsIgnoreCase(in);
+    }
+
+    public static String wordReplacer(String in) {
+        int position = 0;
+        StringBuilder out = new StringBuilder();
+        while (true){
+            int prevPosition = position;
+            position = in.toLowerCase().indexOf("бля", position);
+            if (position != -1) {
+                out.append(in, prevPosition, position).append("блин");
+                position += 3;
+            } else {
+                out.append(in, prevPosition, in.length());
+                break;
+            }
+        }
+        return out.toString();
+    }
+
+    public static int checkRepeats(String str, String subStr) {
+        int counter = 0, position = 0;
+        while (position != -1) {
+            position = str.indexOf(subStr, position);
+            if (position != -1) {
+                counter++;
+                position += subStr.length();
+            }
+        }
+        return counter;
     }
 }
