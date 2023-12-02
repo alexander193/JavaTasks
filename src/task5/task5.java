11fe2df21ccd871280603b9596478176f38f0bcb
alexander193 <sana2001@list.ru>
2023-12-02 13:17:18 +0300

    task5 done

--- src/task5/task5.java
+++ src/task5/task5.java
@@ -1,29 +1,34 @@
 package task5;
 
-
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class task5 {
-    static HashMap<Character, Integer> hashMap = new HashMap<>();
+    static Map<Character, Integer> hashMap = new LinkedHashMap<>();
 
     static {
-        for (int i = 'а'; i <= 'я'; i++)
+        for (int i = 'а'; i <= 'е'; i++)
             hashMap.put((char)i, 0);
         hashMap.put('ё', 0);
+        for (int i = 'ж'; i <= 'я'; i++)
+            hashMap.put((char)i, 0);
     }
-    //
     public static void main(String[] args) {
-        check("абв");
+        check("Привет");
+        check("Как дела?");
         for (Map.Entry<Character, Integer> entry : hashMap.entrySet())
             System.out.println(entry.getKey() + " " + entry.getValue());
     }
 
     static void check(String str) {
+        str = str.toLowerCase();
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
-            int value = hashMap.get(character);
-            hashMap.put(character, ++value);
+            try {
+                int value = hashMap.get(character);
+                hashMap.put(character, ++value);
+            }
+            catch (Exception ignored) {}
         }
     }
 }
