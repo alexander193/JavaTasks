fc77a64c68c83a5d830bd616ea58821d07e4c2a1
alexander193 <sana2001@list.ru>
2023-12-04 00:31:43 +0300

    task6 done

---
+++ src/task6/Converter.java
@@ -0,0 +1,5 @@
+package task6;
+
+public interface Converter {
+    double convert(double celsiusValue);
+}
---
+++ src/task6/task6.java
@@ -0,0 +1,31 @@
+package task6;
+
+public class task6 {
+    public static void main(String[] args) {
+        double celsiusValue = 20;
+        System.out.println(new CelsiusConverter().convert(celsiusValue));
+        System.out.println(new FahrenheitConverter().convert(celsiusValue));
+        System.out.println(new KelvinConverter().convert(celsiusValue));
+    }
+}
+
+class CelsiusConverter implements Converter {
+    @Override
+    public double convert(double celsiusValue) {
+        return celsiusValue;
+    }
+}
+
+class FahrenheitConverter implements Converter {
+    @Override
+    public double convert(double celsiusValue) {
+        return celsiusValue * 1.8 + 32;
+    }
+}
+
+class KelvinConverter implements Converter {
+    @Override
+    public double convert(double celsiusValue) {
+        return celsiusValue + 273.15;
+    }
+}
